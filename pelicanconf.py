9a7209b2a03e0f7956742951da295e68e6d2580e
Stephen McGruer <stephen.mcgruer@gmail.com>
2014-04-13 19:58:24 -0400

    Use octopress theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'pelican-octopress-theme/'
+PLUGIN_PATH = 'pelican-plugins'
