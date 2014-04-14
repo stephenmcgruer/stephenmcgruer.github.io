fb1eafc8d0abd192ca634dbbbf8a73a3abb6fbea
Stephen McGruer <stephen.mcgruer@gmail.com>
2014-04-13 20:15:29 -0400

    Config settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
 AUTHOR = u'Stephen McGruer'
+
 SITENAME = u'Stephen McGruer'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+MENUITEMS = [('Home', '/'), ('About', '/about.html')]
+DISPLAY_CATEGORIES_ON_MENU = False
+
+TIMEZONE = 'America/Toronto'
 
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
 
 THEME = 'pelican-octopress-theme/'
 PLUGIN_PATH = 'pelican-plugins'
+PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.include_code', 'liquid_tags.notebook',
+           'liquid_tags.literal']
+
+TWITTER_USER = 'stephenmcgruer'
+GOOGLE_PLUS_USER = 'stephenmcgruer'
+GOOGLE_PLUS_ONE = True
+GOGOLE_PLUS_HIDDEN = False
+FACEBOOK_LIKE = False
+TWITTER_TWEET_BUTTON = True
+TWITTER_LATEST_TWEETS = True
+TWITTER_FOLLOW_BUTTON = True
+TWITTER_TWEET_COUNT = 3
+TWITTER_SHOW_REPLIES = 'False'
+TWITTER_SHOW_FOLLOWER_COUNT = 'true'
+
+FEED_DOMAIN = None
+FEED_ATOM = None
+
+SEARCH_BOX = True
